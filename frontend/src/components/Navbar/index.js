1a9a9129db4159b9b19845a6f41503c2fe9f0be7
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2022-03-27 17:17:49 +0530

    ghv

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -3,7 +3,27 @@ import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import ConnectWallet from '../connectWallet'
 import './index.scss'
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import { contracts } from '../../utils';
 
+const faucet = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    console.log(signer)
+
+    const faucet = new ethers.Contract(
+        contracts.FAUCET.address,
+        contracts.FAUCET.abi,
+        signer
+    );
+
+    const faucetSpt = await faucet.receive_test_token();
+    console.log(faucetSpt)
+
+};
 
 const NavigationBar = () => {
     const dispatch = useDispatch();
@@ -25,7 +45,7 @@ const NavigationBar = () => {
                         <Nav className="me-auto">
                         </Nav>
                         <Nav>
-                            <Nav.Link href="" className='wallet_address_container'><div>SPT Token &nbsp; </div> <div> Faucet</div></Nav.Link>
+                            <Nav.Link href="" className='wallet_address_container'><div>SPT Token &nbsp; </div> <div onClick={faucet}> Faucet</div></Nav.Link>
 
                             {userBalance > 0 && (
                                 <>
