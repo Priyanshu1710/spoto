d385e60b85a040fd96c046f33c74a4ea313034b8
Priyanshu1710 <guptapriyanshu1710@gmail.com>
2022-04-01 02:57:39 +0530

    Fixed Select Profile Page.

--- frontend/src/pages/SelectProfile/index.js
+++ frontend/src/pages/SelectProfile/index.js
@@ -23,6 +23,10 @@ const SelectProfile = () => {
     const [profileArray, setProfileArray] = useState([])
     const [filteredProfileArray, setFilteredProfileArray] = useState([]);
     const [userHex, setUserHex] = useState();
+    const [userData, setUserData] = useState({
+        _hex: [],
+        profile: []
+    })
 
     const listTokenIds = async () => {
         const web3Modal = new Web3Modal();
@@ -37,55 +41,43 @@ const SelectProfile = () => {
         );
         const tokenIds = await Nftprofile.walletOfOwner(requestAccount());
         console.log(tokenIds);
+        // tokenIds.map(async (item, index) => {
+        //     item.map((singleItem) => {
+        //         console.log("Inside map-->", singleItem);
+        //         let _hex = singleItem._hex;
+        //         console.log(_hex);
+        //     })
+        //     const ipfs = Nftprofile.tokenURI(tokenIds[index]);
 
-        tokenIds.map(async (item, index) => {
-            const ipfs = await Nftprofile.tokenURI(tokenIds[index]);
-            let _hex = item._hex;
-            const newDs = { profile: ipfs, _hex: _hex }
-            return (
-                setIpfsLink((data) => [...data, newDs]),
-                setprofileLink((data) => [...data, newDs.profile])
-            )
-            // const newDs = { profile: ipfs, _hex: _hex }
-            // return setIpfsLink([...ipfsLink, newDs]);
+        //     // console.log(_hex);
+        //     // const newDs = { _hex: _hex }
+        //     console.log(index);
+        //     console.log("tikenId Map-->", item);
+        //     // setIpfsLink((data) => [...data, newDs])
 
-
-            // console.log(ipfs)
-            // console.log("yg", ipfsLink);
-
-            // console.log("Awatghv", response);
-
-            // for (var i = 0; i < tokenIds.length; i++) {
-            //     const ipfs = await Nftprofile.tokenURI(tokenIds[i]).then((response) => {
-            //         console.log("Awatghv", response);
-
-            //         setIpfsLink(i);
-            //         console.log("test--->", ipfsLink);
-
-            //     });
-            //     console.log(ipfs);
-
-            // }
-
-            // const userProfileLink = fetch(ipfsLink);
-            // userProfileLink = userProfileLink.JSON();
-            // console.log("test--->", ipfsLink);
-        })
-        // ipfsLink.map((item, index) => {
-        //     let data = fetch(item.profile).then((response) => response.json());
-        //     console.log("data", data);
-        //     setprofileLink(data);
-
+        //     return (
+        //         // setIpfsLink((data) => [...data, newDs]),
+        //         // setprofileLink((data) => [...data, newDs.profile])
+        //         item
+        //     )
         // })
-        // console.log(ipfsLink[0].profile);
-        // fetProfile();
-
-
+        // tokenIds.forEach((item, index) => {
+        tokenIds[0].forEach((singleItem) => {
+            console.log("InSide forEach-=>", singleItem._hex);
+            setUserData({ ...userData }, userData._hex.push(singleItem._hex))
+        })
+        tokenIds[1].forEach((singleItem) => {
+            console.log("InSide forEach-=>", singleItem);
+            setUserData({ ...userData }, userData.profile.push(singleItem))
+        })
+        // console.log(item);
+        // })
     }
 
-    console.log("ipfs data---->", ipfsLink);
-    console.log("Profile", profileLink);
-    console.log("ProfileArray", profileArray);
+    // console.log("ipfs data---->", ipfsLink);
+    // console.log("Profile", profileLink);
+    // console.log("ProfileArray", profileArray);
+    console.log("UserData", userData);
 
 
 
@@ -93,9 +85,7 @@ const SelectProfile = () => {
         let profile;
         let response;
         let profileData
-        profileLink.map(async (item) => {
-            // profile = profileLink[item]
-            // console.log(profile)
+        userData.profile.map(async (item) => {
             response = await fetch(item)
             profileData = await response.json()
             console.log("user Profile------>", profileData)
@@ -105,27 +95,24 @@ const SelectProfile = () => {
     }
 
     function returnUserHex(value) {
-        ipfsLink.map((item, index) => {
+        userData._hex.map(async (item, index) => {
+            // console.log(item);
             if (index === value) {
-                // return setUserHex(item._hex);
+                console.log(item, index);
                 return (
-                    console.log("in map func", item._hex),
-                    dispatch(setUserHexValue(item._hex))
+                    dispatch(setUserHexValue(item))
                 )
-
             }
             console.log("selected value", value);
         })
     }
 
     useEffect(() => {
-        listTokenIds()
+        listTokenIds().then((res) => {
+            console.log(res);
+            fetProfile()
+        })
     }, [])
-    // useEffect(() => {
-    //     // listTokenIds()
-    //     fetProfile();
-
-    // }, [profileLink,])
 
 
     const { Meta } = Card;
@@ -151,27 +138,10 @@ const SelectProfile = () => {
                                 <div className="frame_bg">
                                     <div className="content_main_container">
                                         <div className="select_sport_container">
-                                            {/* <Card
-                                                hoverable
-                                                style={{ width: 200, height: 200, border: "2px solid #ce18c5" }}
-                                                cover={<img alt="example" src="https://media.istockphoto.com/photos/football-in-the-sunset-picture-id533861572?b=1&k=20&m=533861572&s=170667a&w=0&h=BnEJndSSxMFdAczWGC_ICPEjYG3ce_hep6maCR8xIF8=" />}
-                                                className='sportPage_card'
-                                            >
-                                                <Meta title="Prlayer Id" />
-                                            </Card>
-                                            <Card
-                                                hoverable
-                                                style={{ width: 200, height: 200, border: "2px solid #ce18c5" }}
-                                                cover={<img alt="example" src="https://wallpaperaccess.com/full/1088597.jpg" />}
-                                                className='sportPage_card'
-                                            >
-                                                <Meta title="Prayer Id" />
-                                            </Card> */}
-
                                             {profileArray.map((item, index) => {
                                                 return (
                                                     <React.Fragment key={index}>
-                                                        {/* {item.UserName == "kk" && ( */}
+
                                                         <Link to="/selectSport" ><Card
                                                             hoverable
                                                             style={{ width: 200, height: 200, border: "2px solid #ce18c5" }}
@@ -182,39 +152,17 @@ const SelectProfile = () => {
                                                             <Meta title={item.UserName} />
                                                         </Card></Link>
 
-                                                        {/* )} */}
-
                                                     </React.Fragment>
                                                 )
                                             })}
 
                                         </div>
                                         <div className="coming_soon_cards " >
-                                            {/* <Card title="Create Profile">
-                                                <Card.Grid style={gridStyle}>
-                                                    <Nav onClick={listTokenIds}>
-                                                        <Nav.Link href="">+</Nav.Link>
-                                                    </Nav>
-                                                
-                                                </Card.Grid>
-                                            </Card>
-                                            <Card >
-                                                <Card.Grid style={gridStyle}>
-                                                    <Nav onClick={fetProfile}>
-                                                        <Nav.Link href="">Show</Nav.Link>
-                                                    </Nav>
-                                                </Card.Grid>
-                                            </Card> */}
-                                            <div className="title">Create Profile</div>
+                                            <div className="title">{userData.profile <= 0 && "Please Create Profile"}</div>
                                             <div className="btn_main_container">
                                                 <Link to='/profile'><div className="button_container">+</div></Link>
-                                                <div className="button_container" onClick={fetProfile}>Show Profile</div>
-                                                <button onClick={listTokenIds}>test</button>
                                             </div>
-
                                         </div>
-                                        {/* <button onClick={listTokenIds}>test</button> */}
-
                                     </div>
                                 </div>
                             </div>
